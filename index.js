6fc03732b0412df429245bdb9cbca86e64cdb52c
Ana Holmen <aholmen@Anas-MacBook-Air.local>
2024-01-10 12:06:05 -0600

    new message

---
+++ index.js
@@ -0,0 +1,189 @@
+/* 
+   lit-html snippet - Begin
+   Add to the top of your code. Works with html or jsx!
+   Formats html in a template literal  using the lit-html library 
+   Syntax: html`<div> html or jsx here! ${variable} </div>`
+*/
+//lit-html snippet - Begin
+
+let html = (strings, ...values) =>
+  strings.reduce((acc, str, i) => acc + str + (values[i] || ""), "");
+//lit-html snippet - End
+
+class House {
+  constructor(name) {
+    this.name = name;
+    this.rooms = [];
+  }
+
+  addRoom(name, area) {
+    this.rooms.push(new Room(name, area));
+  }
+}
+
+class Room {
+  constructor(name, area) {
+    this.name = name;
+    this.area = area;
+  }
+}
+
+class HouseService {
+  //   static url = "https://ancient-taiga-31359.herokuapp.com/api/houses";
+
+  static url = "https://659c8892633f9aee7907b1bf.mockapi.io/week12API/Houses";
+
+  static getAllHouses() {
+    return $.get(this.url);
+  }
+
+  static createHouse(name) {
+    return $.post(this.url, { name });
+  }
+
+  static updateHouse(house) {
+    console.log("update house", house);
+    let api = $.ajax({
+      url: `${this.url}/${house.id}/rooms`,
+      dataType: "json",
+      data: JSON.stringify(house.rooms),
+      contentType: "application/json",
+      type: "PUT",
+    });
+    console.log("api", api);
+    return api;
+  }
+
+  static deleteHouse(id) {
+    return $.ajax({
+      url: `${this.url}/${id}`,
+      type: "DELETE",
+    });
+  }
+}
+
+class DOMManager {
+  static houses;
+
+  static getAllHouses() {
+    HouseService.getAllHouses().then((houses) => this.render(houses));
+  }
+
+  static createHouse(name) {
+    HouseService.createHouse(name)
+      .then(() => HouseService.getAllHouses())
+      .then((houses) => this.render(houses));
+  }
+
+  static deleteHouse(id) {
+    HouseService.deleteHouse(id)
+      .then(() => HouseService.getAllHouses())
+      .then((houses) => this.render(houses));
+  }
+
+  static addRoom(id) {
+    console.log("addroom...", id);
+    for (let house of this.houses) {
+      if (house.id == id) {
+        console.log("house", house.id);
+        house.rooms.push(
+          new Room(
+            $(`#${house.id}-room-name`).val(),
+            $(`#${house.id}-room-area`).val()
+          )
+        );
+        console.log("house object", house);
+        HouseService.updateHouse(house)
+          .then(() => HouseService.getAllHouses())
+          .then((houses) => this.render(houses));
+      }
+    }
+  }
+
+  static deleteRoom(houseId, roomId) {
+    for (let house of this.houses) {
+      if (house.id == houseId) {
+        for (let room of house.rooms) {
+          if (room.id == roomId) {
+            house.rooms.splice(house.rooms.indexOf(room), 1);
+            HouseService.updateHouse(house)
+              .then(() => {
+                HouseService.getAllHouses();
+              })
+              .then((houses) => this.render(houses));
+          }
+        }
+      }
+    }
+  }
+
+  static render(houses) {
+    this.houses = houses;
+    $("#app").empty();
+    for (let house of houses) {
+      $("#app").prepend(html`
+        <div id="${house.id}" class="card">
+          <div class="card-header">
+            <h2>${house.name}</h2>
+            <button
+              class="btn btn-danger"
+              onclick="DOMManager.deleteHouse('${house.id}')"
+            >
+              Delete
+            </button>
+          </div>
+          <div class="card-body">
+            <div class="card">
+              <div class="row">
+                <div class="col-sm">
+                  <input
+                    type="text"
+                    id="${house.id}-room-name"
+                    class="form-control"
+                    placeholder="room name"
+                  />
+                </div>
+                <div class="col-sm">
+                  <input
+                    type="text"
+                    id="${house.id}-room-area"
+                    class="form-control"
+                    placeholder="room area"
+                  />
+                </div>
+              </div>
+              <button
+                id="${house.id}-new-room"
+                onclick="DOMManager.addRoom('${house.id}')"
+                class="btn btn-primary form-control"
+              >
+                Add
+              </button>
+            </div>
+            <br />
+            <div id="rooms-${house.id}">
+              <!-- Display rooms here -->
+            </div>
+          </div>
+        </div>
+      `);
+
+      for (let room of house.rooms) {
+        $(`#rooms-${house.id}`).append(`
+            <p>
+              <strong>Name:</strong> ${room.name}<br>
+              <strong>Area:</strong> ${room.area}<br>
+              <button class="btn btn-danger" onclick="DOMManager.deleteRoom('${house.id}', '${room.id}')">Delete Room</button>
+            </p>
+          `);
+      }
+    }
+  }
+}
+
+$("#create-new-house").click(() => {
+  DOMManager.createHouse($("#new-house-name").val());
+  $("#new-house-name").val("");
+});
+
+DOMManager.getAllHouses();
